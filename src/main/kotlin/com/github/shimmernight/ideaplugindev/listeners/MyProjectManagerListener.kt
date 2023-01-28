c79fa6b5bab32347090956f2aa184bbaca021250
GitHub Action <action@github.com>
2023-01-28 10:04:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/shimmernight/ideaplugindev/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/shimmernight/ideaplugindev/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.shimmernight.ideaplugindev.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.shimmernight.ideaplugindev.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/shimmernight/ideaplugindev/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/shimmernight/ideaplugindev/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.shimmernight.ideaplugindev.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shimmernight.ideaplugindev.MyBundle
 
 class MyApplicationService {
 
