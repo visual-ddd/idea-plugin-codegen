3c56fec103f71da541a5dd6226c9a6a275c05109
ZhuXueLiang <ZhuXueLiang@wakedata.com>
2024-01-23 14:17:12 +0800

    style:升级代码生成器组件版本号4.0.1-RC02&升级版本号1.2.2-RC01

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.wd.paas:wd-generator-core:4.0.1-RC01") {
+    implementation("com.wd.paas:wd-generator-core:4.0.1-RC02") {
         exclude(group = "org.apache.velocity", module = "velocity")
         exclude(group = "org.apache.velocity", module = "velocity-engine-core")
     }
